279d2b60c6f126fd0142081e4b060e985b94f721
Eder  Abreu <elopes.abreu@gmail.com>
2018-02-06 21:44:32 -0200

    Change 1

--- variaveis/exemplo-02.php
+++ variaveis/exemplo-02.php
@@ -8,17 +8,14 @@ $nome2 = "Pedro";
 $nomeComposto = $nome1 . " " . $nome2;
 
 echo $nomeComposto;
-
 echo "</br>";
-
 unset($anoNascimento);
 
 if(isset($anoNascimento)){
  echo $anoNascimento;	
 }else{
+    
  echo "variavel indefinida";
 }
-
-
 
 ?>
